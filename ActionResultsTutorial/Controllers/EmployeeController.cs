6e641d26b5d505ba002b028f5af1987fdbd8fcbd
Shirinov Nurlan Anar <shirinov_n@itstep.edu.az>
2025-01-19 14:48:03 +0400

    some

---
+++ ActionResultsTutorial/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using ActionResultsTutorial.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace ActionResultsTutorial.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //Eger methodda [HTTPGET] tag i yazilmiyibsa default olaraq get dir
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var vm = new EmployeeAddViewModel()
+            {
+                Employee = new Entities.Employee(),
+                Cities = new List<SelectListItem>
+                {
+                    new SelectListItem {Text = "Baku" , Value="1"},
+                    new SelectListItem {Text = "Gence" , Value="2"},
+                    new SelectListItem {Text = "Sumqayit" , Value="3"},
+                    new SelectListItem {Text = "Agdam" , Value="4"},
+                }
+            };
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Add(EmployeeAddViewModel viewModel)
+        {
+            var vm = new EmployeeAddViewModel()
+            {
+                Employee = new Entities.Employee(),
+                Cities=new List<SelectListItem> { }
+            };
+            return View(vm);
+        }
+    }
+}
--- ActionResultsTutorial/Entities/Employee.cs
+++ ActionResultsTutorial/Entities/Employee.cs
@@ -1,12 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ActionResultsTutorial.Entities
 {
     public class Employee
     {
+        [Range(1,100)]
         public int Id { get; set; }
+
+        [DisplayName("User Name")]
+        [Required]
         public string Firstname { get; set; }
+
+        [DisplayName("User Surname")]
+        [Required]
         public string Lastname { get; set; }
+
         public int CityId { get; set; }
     }
 }
